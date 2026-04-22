a736f65a41e1233f656efa0857356772d6348764
Russel Arbore <russel.jma@gmail.com>
2026-04-22 13:48:47 -0700

    Ifelse case

--- opt-rw/src/ssa.rs
+++ opt-rw/src/ssa.rs
@@ -17,14 +17,12 @@ define_language! {
     }
 }
 
-pub type Edge = (BlockId, Id);
-
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum Block {
     Start,
-    Child(Edge),
-    Merge(Edge, Edge),
-    Return(Edge, Id),
+    Child(BlockId, Id),
+    Merge(BlockId, BlockId),
+    Return(BlockId, Id),
 }
 
 pub type DFG = EGraph<SSA, ()>;
@@ -35,18 +33,21 @@ struct SSACtx<'a, 'b> {
     vars: FxHashMap<&'a str, Id>,
     dfg: &'b RefCell<DFG>,
     cfg: &'b RefCell<CFG>,
-    curr_block: BlockId,
+    block_hashcons: &'b RefCell<FxHashMap<Block, BlockId>>,
+    last_block: BlockId,
 }
 
 pub fn optimistic_rewriting(func: &FuncAST) -> (DFG, CFG) {
     let dfg = RefCell::new(EGraph::new(()));
     let cfg = RefCell::new(CFG::default());
+    let block_hashcons = RefCell::new(FxHashMap::default());
 
     let mut ctx = SSACtx {
         vars: FxHashMap::default(),
         dfg: &dfg,
         cfg: &cfg,
-        curr_block: 0,
+        block_hashcons: &block_hashcons,
+        last_block: 0,
     };
     for (idx, name) in func.params.iter().enumerate() {
         ctx.vars.insert(name, dfg.borrow_mut().add(SSA::Param(idx)));
@@ -59,10 +60,24 @@ pub fn optimistic_rewriting(func: &FuncAST) -> (DFG, CFG) {
 
 impl<'a, 'b> SSACtx<'a, 'b> {
     fn add_block(&self, block: Block) -> BlockId {
-        let mut cfg = self.cfg.borrow_mut();
-        let id = cfg.len();
-        cfg.push(block);
-        id
+        if let Some(id) = self.block_hashcons.borrow().get(&block) {
+            *id
+        } else {
+            let mut cfg = self.cfg.borrow_mut();
+            let id = cfg.len();
+            cfg.push(block);
+            self.block_hashcons.borrow_mut().insert(block, id);
+            id
+        }
+    }
+
+    fn set_block(&self, block: Block, id: BlockId) {
+        assert!(id < self.cfg.borrow().len());
+        let old_block = self.cfg.borrow()[id];
+        let old_id = self.block_hashcons.borrow_mut().remove(&old_block).unwrap();
+        assert_eq!(old_id, id);
+        self.cfg.borrow_mut()[id] = block;
+        self.block_hashcons.borrow_mut().insert(block, id);
     }
 
     fn handle_stmt(self, stmt: &'a StmtAST) -> Option<Self> {
@@ -102,8 +117,32 @@ impl<'a, 'b> SSACtx<'a, 'b> {
         let true_cond = self.handle_expr(cond);
         let false_cond = self.mk(SSA::Unary(UnaryOp::Not, true_cond));
         let one = self.mk(SSA::Constant(1));
+        let mut then_ctx = None;
+        let mut else_ctx = None;
 
-        todo!()
+        if !self.is_always_false(true_cond) {
+            let then_block = self.add_block(Block::Child(self.last_block, true_cond));
+            let mut ctx = self.clone();
+            ctx.last_block = then_block;
+            then_ctx = ctx.handle_stmt(then_stmt);
+        }
+
+        if !self.is_always_false(false_cond) {
+            let else_block = self.add_block(Block::Child(self.last_block, false_cond));
+            let mut ctx = self.clone();
+            ctx.last_block = else_block;
+            else_ctx = ctx.handle_stmt(else_stmt);
+        }
+
+        match (then_ctx, else_ctx) {
+            (Some(then_ctx), Some(else_ctx)) => {
+                let block = self.add_block(Block::Merge(then_ctx.last_block, else_ctx.last_block));
+                self.merge_vars(&then_ctx, &else_ctx, block);
+                self.last_block = block;
+                Some(self)
+            }
+            (ctx, None) | (None, ctx) => ctx,
+        }
     }
 
     fn handle_while(mut self, cond: &ExprAST, stmt: &'a StmtAST) -> Option<Self> {
@@ -112,11 +151,17 @@ impl<'a, 'b> SSACtx<'a, 'b> {
 
     fn handle_return(self, expr: &ExprAST) -> Option<Self> {
         let value = self.handle_expr(expr);
-        self.add_block(Block::Return(
-            (self.curr_block, self.mk(SSA::Constant(1))),
-            value,
-        ));
+        self.add_block(Block::Return(self.last_block, value));
         None
+    }
+
+    fn merge_vars(&mut self, a: &Self, b: &Self, block: BlockId) {
+        for (name, a_value) in &a.vars {
+            if let Some(b_value) = b.vars.get(name) {
+                self.vars
+                    .insert(name, self.mk(SSA::Phi(block, [*a_value, *b_value])));
+            }
+        }
     }
 
     fn handle_expr(&self, expr: &ExprAST) -> Id {
@@ -134,5 +179,9 @@ impl<'a, 'b> SSACtx<'a, 'b> {
 
     fn mk(&self, value: SSA) -> Id {
         self.dfg.borrow_mut().add(value)
+    }
+
+    fn is_always_false(&self, value: Id) -> bool {
+        self.mk(SSA::Constant(0)) == value
     }
 }
