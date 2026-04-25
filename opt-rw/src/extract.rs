56fb75036b12119255229b8b1346864ebc977e73
Russel Arbore <russel.jma@gmail.com>
2026-04-25 16:23:21 -0700

    Comment the extraction code

--- opt-rw/src/extract.rs
+++ opt-rw/src/extract.rs
@@ -4,6 +4,7 @@ use std::collections::BinaryHeap;
 use egg::{Analysis, EGraph, Id, Language};
 use rustc_hash::{FxHashMap, FxHashSet};
 
+// std::collections::BinaryHeap is a max heap, we want a min heap, so do a custom order.
 #[derive(Debug, Clone, PartialEq, Eq)]
 struct QueueElement<L: Language> {
     node: L,
@@ -38,6 +39,10 @@ where
     let mut parents: FxHashMap<Id, Vec<L>> = FxHashMap::default();
     let mut num_unknown_children: FxHashMap<L, usize> = FxHashMap::default();
 
+    // Initialize data structures:
+    // 1. Count the number of unique children (e-class IDs) per node.
+    // 2. Assemble the "parents" (user nodes) of each e-class ID.
+    // 3. Put leaf nodes into priority queue.
     let mut child_dedup: FxHashSet<Id> = FxHashSet::default();
     for class in egraph.classes() {
         for node in class.iter() {
@@ -49,6 +54,9 @@ where
                 parents.entry(child).or_default().push(node.clone());
             }
 
+            // Assuming the cost function is "superior" in Knuth's parlance, starting off by just
+            // inserting leaf nodes into the priority queue won't miss out on potentially cheaper
+            // extraction candidates.
             if node.is_leaf() {
                 let cost = cf(&node, &costs);
                 priority.push(QueueElement {
@@ -61,17 +69,24 @@ where
 
     while let Some(QueueElement { node, cost }) = priority.pop() {
         let id = egraph.lookup(node.clone()).unwrap();
+        // Due to the Dijkstra style extraction, if we've found an extraction of an e-class, it is
+        // already the best.
         if costs.contains_key(&id) {
             continue;
         }
 
+        // Once an e-class has a cost, that cost won't get better later. Transitively, the cost of
+        // nodes also won't change once its children e-classes have been extracted.
         assert_eq!(cost, cf(&node, &costs));
         costs.insert(id, (cost, node));
         for parent in parents.entry(id).or_default() {
+            // A parent of an e-class may be in an e-class we've already extracted.
             if costs.contains_key(&egraph.lookup(parent.clone()).unwrap()) {
                 continue;
             }
 
+            // Once all of the children of a parent node (including the current child) have been
+            // visited, that node's cost can be calculated and pushed onto the queue.
             let num_unknown = num_unknown_children.get_mut(parent).unwrap();
             *num_unknown -= 1;
             if *num_unknown == 0 {
@@ -145,5 +160,19 @@ fn test(x) return 2 * x;
             panic!()
         };
         assert_eq!(costs[&id].0, 9);
+    }
+
+    #[test]
+    fn extract2() {
+        let program = r#"
+fn test(x) return ((2 * x) - (2 * x)) * ((2 * x) - (2 * x));
+"#;
+        let parsed = ProgramParser::new().parse(&program).unwrap();
+        let (dfg, cfg) = optimistic_rewriting(&parsed[0]);
+        let costs = extract(&dfg, placeholder_cost);
+        let Block::Return(_, id) = cfg[1] else {
+            panic!()
+        };
+        assert_eq!(costs[&id].0, 66);
     }
 }
